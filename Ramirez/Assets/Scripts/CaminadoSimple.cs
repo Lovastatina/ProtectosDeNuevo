449c6d94f54d70cfb54739876ee5d542f6fc2ce5
Lovastatina <142038796+Lovastatina@users.noreply.github.com>
2024-01-13 09:32:54 -0600

    callao

--- Ramirez/Assets/Scripts/CaminadoSimple.cs
+++ Ramirez/Assets/Scripts/CaminadoSimple.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CaminadoSimple : MonoBehaviour
 {
+    public int velCam  = 0 ;
+    private Rigidbody miCorpo;
+    private Animator miAnimador;
     // Start is called before the first frame update
     void Start()
     {
-        
+        miCorpo = GetComponent<Rigidbody>();
+        miAnimador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float movH = Input.GetAxis("Vertical");
         
+        if(movH > 0)
+        {
+            miCorpo.velocity = new Vector3(0, 0, velCam);
+            miAnimador.SetBool("Cam", true);
+        }
+
+        else if (movH <= 0)
+        {
+            miCorpo.velocity = new Vector3(0, 0, 0);
+            miAnimador.SetBool("Cam", false);
+        }
     }
 }
